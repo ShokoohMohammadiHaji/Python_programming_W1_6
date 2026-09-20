4743e6d533ac0b727924d67c1324d738522797a1
shokooh <shokooh.mohammadi@gmail.com>
2026-09-20 12:19:21 +0300

    W1_6

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-Feed = input("Insert an integer: ")
+Feed = input("Insert an integer:")
 Value = int(Feed)
 Remainder = Value % 2
 print(f"Value is {Value}")
-print(f"Theremainder is {Remainder} when {Value} is divided by 2.")
+print(f"The remainder is {Remainder} when {Value} is divided by 2.")
